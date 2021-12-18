78c4dc6f8d2478f0c61a3af5974a1b4c88967ad8
nilsmo1 <nilsmo1@hotmail.com>
2021-12-18 14:46:20 +0100

    Registration System

--- C/RegistrationSystem.cpp
+++ C/RegistrationSystem.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
 #include <string>
-#include <set>
 #include <vector>
+#include <algorithm>
+#include <map>
 using namespace std;
 using ll = int64_t;
 #define endline '\n';
-// Time limit exceeded 
-int in(string s, set<string> ss) {
-    return (ss.find(s) != ss.end());
-}
 
 int main() {
     ll n; 
     cin >> n;
-    set<string> ss;
-    vector<string> rs;
+    map<string, int> ss;
     for (ll i=0; i<n; i++) { 
         string s;
         cin >> s;
-        if (!(in(s,ss))) { ss.insert(s); rs.push_back("OK"); }
-        else {
-            ll i = 1;
-            while (1) {
-                if (!(in(s+to_string(i),ss))) { ss.insert(s+to_string(i)); rs.push_back(s+to_string(i)); break; }
-                i++;
-            }
-        }
+        if (ss.find(s) == ss.end()) {
+            ss.insert(pair<string, int>(s, 1));
+            cout << "OK" << endline;
+        } else {
+            cout << s+to_string(ss[s]) << endline;
+            ss[s]++;
+        } 
     }
-    for (string S : rs) { cout << S << endline; }
     return 0;
 }
