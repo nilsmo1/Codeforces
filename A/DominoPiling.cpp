94f77089ef26af29576685091c2dfe6929ed4dcd
Nils <nilsmo1@hotmail.com>
2022-04-12 01:27:51 +0200

    DominoPiling

---
+++ A/DominoPiling.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+using ll = int64_t;
+#define endline '\n';
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    cout << a*b/2 << endline;
+    return 0;
+}
